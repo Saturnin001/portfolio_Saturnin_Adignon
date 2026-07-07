311e57f659388f088f878274de06913f0079462e
Saturnin001 <saturninadignon@gmail.com>
2026-07-07 12:14:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const menuBtn = document.querySelector('.menu-toggle');
+// On prend le bouton vert qui existe déjà
+const menuBtn = document.querySelector('header button, header i, header svg');
 const nav = document.querySelector('nav');
 
 menuBtn.addEventListener('click', () => {
